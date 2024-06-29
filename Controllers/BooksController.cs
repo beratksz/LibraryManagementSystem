c4f9b34a4080fb81787a57d27934d1e8b1a71879
Berat Öksüz <beratksz@icloud.com>
2024-06-29 13:22:08 +0300

    update

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-
+using LibraryManagementSystem.Models;
 
 public class BooksController : Controller
 {
@@ -13,10 +13,129 @@ public class BooksController : Controller
         _context = context;
     }
 
+    // GET: Books/Index
     public async Task<IActionResult> Index()
     {
         return View(await _context.Books.ToListAsync());
     }
 
-    // Create, Edit, Delete, Details işlemleri için gerekli aksiyon metodlarını ekleyin
+    // GET: Books/Details/5
+    public IActionResult Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var book = _context.Books.FirstOrDefault(m => m.Id == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        return View(book);
+    }
+
+    // GET: Books/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Books/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Book book)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(book);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(book);
+    }
+
+    // GET: Books/Edit/5
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var book = _context.Books.Find(id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        return View(book);
+    }
+
+    // POST: Books/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Book book)
+    {
+        if (id != book.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(book);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(book.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(book);
+    }
+
+    // GET: Books/Delete/5
+    public IActionResult Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var book = _context.Books.FirstOrDefault(m => m.Id == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        return View(book);
+    }
+
+    // POST: Books/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var book = _context.Books.Find(id);
+        _context.Books.Remove(book);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool BookExists(int id)
+    {
+        return _context.Books.Any(e => e.Id == id);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
